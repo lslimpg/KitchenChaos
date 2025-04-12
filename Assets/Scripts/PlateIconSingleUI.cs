5a731a76ccf80bb96c84c68f3dd5c2bb0e1bed23
Li-Sha Lim <limxx518@gmail.com>
2025-04-12 16:05:02 -0700

    Added plate world UI

---
+++ Assets/Scripts/PlateIconSingleUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlateIconSingleUI : MonoBehaviour {
+
+    [SerializeField] private Image image;
+    public void SetKitchenObjectSO(KitchenObjectSO kitchenObjectSO) {
+        image.sprite = kitchenObjectSO.sprite;
+    }
+
+}
